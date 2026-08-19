adac315ce41d1d4df6152480198f3a3234fcf479
jhjo <jjh9242@i-data.ai>
2026-08-19 21:08:49 +0900

    fix bugs

--- js/config.js
+++
@@ -1,5 +0,0 @@
-export const SUPABASE_URL = "https://eegoxhznegpwfhcnjcnd.supabase.co";
-export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVlZ294aHpuZWdwd2ZoY25qY25kIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODcwNTM5MjQsImV4cCI6MjEwMjYyOTkyNH0.nGwwF9hw-O-VshC_-tZ3o3aEe5VJHL06YBfVVcbwAkc";
-// Google Cloud Console에서 YouTube Data API v3 키 발급 후 채워 넣을 것.
-// HTTP 리퍼러를 sugardjmusic.kr 로 제한해야 한다.
-export const YOUTUBE_API_KEY = "AIzaSyAJ3A9pYJcQ0r1JFYgmGS0FsdA047imPYw";
